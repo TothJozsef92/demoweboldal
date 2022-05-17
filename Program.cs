7bb2dd65ae8afa9d535d3cf877ed0545a7c5b701
tjozsef10101 <tjozsef10101@gmail.com>
2022-05-17 23:17:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloVCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello VCS!");
+        }
+    }
+}
